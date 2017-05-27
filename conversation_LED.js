35f69df99ac0bd11ac35451ccef4a62d91108c01
Kyle Li <kanyangli@gmail.com>
2017-05-27 03:25:11 +0000

    Add files via upload

---
+++ conversation_LED.js
@@ -0,0 +1,120 @@
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var TJBot = require('tjbot');
+var config = require('./config');
+
+// obtain our credentials from config.js
+var credentials = config.credentials;
+
+// obtain user-specific config
+var WORKSPACEID = config.conversationWorkspaceId;
+
+// these are the hardware capabilities that TJ needs for this recipe
+var hardware = ['microphone', 'speaker'];
+
+// set up TJBot's configuration
+var tjConfig = {
+    log: {
+        level: 'verbose'
+    },
+    robot: {
+	name: '',
+        gender: 'male' 
+    }
+};
+
+// Grovepi declaration
+var GrovePi = require('../../../Desktop/GrovePi/Software/NodeJS/libs').GrovePi;
+var led = new GrovePi.sensors.DigitalOutput(4);
+var board;
+
+// Called on Ctrl-C.
+// Close the board and clean up
+function onExit(err){
+    console.log('Bye, have a nice day!');
+    board.close();
+    process.removeAllListeners();
+    process.exit();
+    if(typeof err != 'undefined'){
+	console.log(err)
+    }
+}
+// initiate a board
+board = new GrovePi.board({
+    debug: true,
+    onError: function(err){
+    	console.log("GrovePi initialization failed: " + err);
+    },
+    onInit: function(res){
+	console.log("GrovePi initialization succeed");
+    }
+})
+// initialize the board
+board.init();
+// Catches ctrl+c event
+process.on('SIGINT', onExit);
+
+// instantiate our TJBot!
+var tj = new TJBot(hardware, tjConfig, credentials);
+
+console.log("You can ask me to introduce myself or tell you a joke.");
+console.log("Try saying, \"" + tj.configuration.robot.name + ", please introduce yourself\" or \"" + tj.configuration.robot.name + ", who are you?\"");
+console.log("You can also say, \"" + tj.configuration.robot.name + ", tell me a joke!\"");
+
+// listen for utterances with our attentionWord and send the result to
+// the Conversation service
+var context = {};
+tj.listen(function(msg) {
+    // check to see if they are talking to TJBot
+    if (msg.startsWith(tj.configuration.robot.name)) {
+        // remove our name from the message
+	var turn = {
+	   workspace_id: WORKSPACEID,
+	   input: { 
+	      'text': msg.toLowerCase().replace(tj.configuration.robot.name.toLowerCase(), "")
+	   }, 
+	   context: context
+	};
+	
+	// send to the conversation service
+	tj._conversation.message(turn, function(err, response){
+	   if(err){
+	      console.log('error:', err);	  
+           }else{
+	      context = response.context;
+		if (response.output.action !== undefined) {
+		    if (response.output.action === "light_on"){
+			led.turnOn();
+		    }else if (response.output.action === "light_off"){
+			led.turnOff();
+		    }
+		}
+	      tj.speak(response.output.text.join(' ').trim());
+           }
+	});	
+    }
+});
+
+
+
+
+
+
+
+
+
+
